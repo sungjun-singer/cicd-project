6cef2e00c5f30083e804c8eaa499b8fcd0a21573
“sungjunkim” <kis07150@naver.com>
2025-02-24 20:07:47 +0900

    update : App.js 구문 업데이트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I Success Git Action + S3 + CloudFront CI/CD!
         </p>
         <a
           className="App-link"
